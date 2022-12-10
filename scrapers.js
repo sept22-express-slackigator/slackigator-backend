103568174fc20ae28676b8e54c52ab6e69809426
Tom Booth <tombeezle@gmail.com>
2022-12-09 16:23:58 -0800

    first scrape successful

--- scrapers.js
+++ scrapers.js
@@ -1,5 +1,18 @@
 const puppeteer = require('puppeteer');
 
 async function scrapePage(url) {
-    const browser = 
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const [el] = await page.$x(
+    '/html/body/div[1]/div[2]/div/div/div[2]/div[1]/div[2]/article/div/header/div/time'
+  );
+  const text = await el.getProperty('textContent');
+  const rawTxt = await text.jsonValue();
+  console.log({ rawTxt });
+
+  browser.close();
 }
+
+scrapePage('https://globalpdx.org/events/list/');
