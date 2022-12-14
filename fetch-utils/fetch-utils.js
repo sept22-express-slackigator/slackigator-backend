51f20cfdd6c9e385a6b44dc0d3ae8d49cb6cf89d
Tom Booth <tombeezle@gmail.com>
2022-12-13 17:15:20 -0800

    route posting

---
+++ fetch-utils/fetch-utils.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+const payload = {
+  channel: 'webhook-tests',
+  blocks: [],
+};
+
+fetch(`https://hooks.slack.com/services.${process.env.SLACK_WEBHOOK_TOKENS}`, {
+  method: 'POST',
+  body: JSON.stringify(payload),
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Content-Length': payload.length,
+    Accept: 'applcation/json',
+  },
+}).then((res) => {
+  if (!res.ok) {
+    throw new Error();
+  }
+});
--- scraper.js
+++ scraper.js
@@ -1,11 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const { startBrowser } = require('./lib/services/scraper/browser.js');
 const scraperObject = require('./lib/services/scraper/pageScraper.js');
-
+const Event = require('./lib/models/Event.js');
+console.log(process.env.DATABASE_URL);
 async function grabScraped() {
   const browser = await startBrowser();
-  const scrapedList = await scraperObject.scraper(browser);
-  console.log('scraped lists', scrapedList);
-  return scrapedList;
+  const events = await scraperObject.scraper(browser);
+  try {
+    await Promise.all(events.map((event) => Event.insert(event)));
+  } catch (e) {
+    console.error(e);
+  }
+  return events;
 }
 grabScraped();
 
