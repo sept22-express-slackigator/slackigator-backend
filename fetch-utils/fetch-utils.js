fdc0e4e0453415c6943f8fc58f95ebbced5685f5
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-12-14 14:06:47 -0800

    comment out fetch-utils

--- fetch-utils/fetch-utils.js
+++ fetch-utils/fetch-utils.js
@@ -76,21 +76,21 @@ console.log(inFetchEvents);
 //   ],
 // };
 
-fetch(`https://hooks.slack.com/services.${process.env.SLACK_WEBHOOK_TOKENS}`, {
-  method: 'POST',
-  body: JSON.stringify(payload),
-  headers: {
-    'Content-Type': 'application/json; charset=utf-8',
-    'Content-Length': payload.length,
-    Accept: 'applcation/json',
-  },
-})
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`SERVER ERROR ${res.status}`);
-    }
-    return res.json();
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// fetch(`https://hooks.slack.com/services.${process.env.SLACK_WEBHOOK_TOKENS}`, {
+//   method: 'POST',
+//   body: JSON.stringify(payload),
+//   headers: {
+//     'Content-Type': 'application/json; charset=utf-8',
+//     'Content-Length': payload.length,
+//     Accept: 'applcation/json',
+//   },
+// })
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw new Error(`SERVER ERROR ${res.status}`);
+//     }
+//     return res.json();
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
