3731608cdb6f1c983be5a1bc6bff297f4394c4f7
Dylan <dylflanagan@gmail.com>
2022-12-15 21:31:47 -0800

    Added pseudocode to all relevant files for revisitation down the line

--- scraper.js
+++ scraper.js
@@ -1,13 +1,21 @@
+// loads environment variables, bypasses limitations of .env
 const dotenv = require('dotenv');
 dotenv.config();
 const { startBrowser } = require('./lib/services/scraper/browser.js');
+
+// nodeCron is a module that allows for setting up tasks in node.js
 const nodeCron = require('node-cron');
+
+// object built out of scraped data
 const scraperObject = require('./lib/services/scraper/pageScraper.js');
 const Event = require('./lib/models/Event.js');
+
+// function for taking scraped data, then inputting it into database
 async function grabScraped() {
   const browser = await startBrowser();
   const events = await scraperObject.scraper(browser);
   try {
+    // await each scraped detail page to settle, before returning data
     await Promise.allSettled(events.map((event) => Event.insert(event)));
   } catch (e) {
     console.error(e);
@@ -15,6 +23,7 @@ async function grabScraped() {
   return events;
 }
 
+// scheduled task (* = second, minute, hour, day of month, month, day of week)
 nodeCron.schedule('*/1 * * * *', async () => {
   await grabScraped();
 });
